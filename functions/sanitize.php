0afe2a4ad9bacf86783297c17fdb1b5f2b7b0e3c
Alex SaM <alsam3637@gmail.com>
2025-05-17 11:00:00 -0400

    Add functions.

--- functions/sanitize.php
+++ functions/sanitize.php
@@ -1,1 +1,9 @@
 <?php
+function escape($string): string {
+    return htmlentities($string, ENT_QUOTES, 'UTF-8');
+}
+
+function print_pre($var, $flag = null): void {
+    echo '<pre>' , print_r($var, 1) . '</pre>';
+    if ($flag) { exit(); }
+}
